e8dbbc1fdb3a37209623bac7ef5d998617aad708
ahlam alshaba <ama1977ln@outlook.com>
2021-02-19 13:01:46 +0100

    added the codes

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,18 @@
 //insert your pseudocode below
-/*
+/*there are to var here so we can creat one for Text
+ and the other one for the number of the characters
+and ask the user to print a text ex : Print text please 
+the last step print the text and the numbers
 
 here
 
 */
 
 //insert your code below
+var text, number_text;
+
+
+text = window.prompt('Type your text please');
+number_text = text.length;
+window.alert(number_text);
+window.alert(text);
