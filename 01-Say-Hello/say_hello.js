a4a0198f2ad54b110b6ae7f2f5016c1d71a3c91d
ahlam alshaba <ama1977ln@outlook.com>
2021-02-19 12:23:20 +0100

    added the code

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,14 @@
 //insert your pseudocode below
-/*
-
+/*we 2 var the first is name the second the greatting 
+abd ask the user to put his name ex: enter your name.
 here
 
 */
 
 //insert your code below
+var greeting, name2;
+
+
+greeting = 'Welcome ';
+name2 = window.prompt('Enter your name please');
+window.alert(greeting + name2);
